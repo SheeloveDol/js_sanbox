91b3caade20126e15ff09ffd67ad81892fccb700
Sheelove Dol <siesel26@gmail.com>
2021-10-28 22:18:14 -0700

    Solution to isSortedandhow()

--- isSorted.js/main.js
+++ isSorted.js/main.js
@@ -3,24 +3,27 @@ let ascending = [1, 2, 3, 4, 5];
 let descending = [10, 9, 8, 7, 6];
 let random = [11, 13, 12, 15, 14];
 
-const sorted = arr => {
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] < arr[i + 1]) {
-      return 'Yes, ascending'
-    } else if (arr[i] > arr[i + 1]) {
-      return 'Yes, descending'
-    } else {
-      return 'No'
-    }
-  }
+const sorted = array => {
+  let up = array.filter((e, i, arr) => e > arr[i + 1]).length == 0;
+  let down = array.filter((e, i, arr) => e < arr[i + 1]).length == 0;
+  
+  return up ? 'Yes, ascending' : down ? 'Yes, descending' : 'no';
 
 }
+
+
 
 console.log(sorted(ascending));
 console.log(sorted(descending))
 
 console.log(sorted(random))
 
-let test = random.sort((a, b) => a - b)
-console.log(test)
-console.log(random)
+// let test = random.sort((a, b) => a - b)
+// console.log(test)
+// console.log(random)
+
+
+// Testing out the filter method to better understand how to solve this.
+
+// let test = random.filter(el => el > 11).length == 0;
+// console.log(test)
