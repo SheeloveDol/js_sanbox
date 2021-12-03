9b941113660e93567c87f695b5784c5d36859100
Sheelove Dol <siesel26@gmail.com>
2021-12-02 22:23:12 -0800

    Worked on a few more things while at work

--- counting-dupes.js
+++ counting-dupes.js
@@ -1,17 +1,60 @@
-const newString = 'Hope I am not here very late';
+// const newString = 'Hope I am not here very late';
 
-let split = newString.split('');
-console.log(split)
+// let split = newString.split('');
+// console.log(split)
 
-let filtered = split.filter(item => item !== ' ')
+// let filtered = split.filter(item => item !== ' ')
 
-console.log(filtered)
-//   let ready = item.split('').join('').sort((a, b) => a - b)
-//   return ready;
+// console.log(filtered)
+// //   let ready = item.split('').join('').sort((a, b) => a - b)
+// //   return ready;
+// // }
+
+// // console.log(sorting(newString))
+// console.log(' ')
+
+// let sorted = filtered.sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }))
+// console.log(sorted);
+
+// let newCount = 0
+
+// let lowerCased = sorted.forEach((letter, i) => {
+//   console.log(letter.toLowerCase())
+//   // if (letter[i] === letter[i + 1]) {
+//   //   newCount += 1
+  
+//   // }
+
+//   // console.log(newCount)
+// })
+// console.log(lowerCased)
+// /* 
+//   1. Lopp through the array
+//   2. take first element item[i].toLowerCase() compare it item[i + 1].toLowerCase()
+//   3. if they are the same, increase count 
+// */
+
+
+// let count = 0
+
+// for (let i = 0; i < sorted.length; i++) {
+//   // let count = 0
+//   if (sorted[i] === sorted[i + 1]) {
+//     count += 1;
+//     sorted = sorted.filter(item => item !== sorted[i])
+//   }
+//   // console.log(sorted)
 // }
 
-// console.log(sorting(newString))
-console.log(' ')
+// console.log(count)
 
-let sorted = filtered.sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }))
-console.log(sorted);
+const lettersArr = ['a', 'b', 'c']
+
+ const okay = lettersArr.map(letter => console.log(letter.toUpperCase()))
+
+
+const a = 'b'
+
+console.log(a.toUpperCase())
+console.log(lettersArr)
+console.log(okay)
