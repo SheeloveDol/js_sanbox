2c358fa51a5a3701d3f0daccb3d7aeb07cca5fa5
Sheelove Dol <siesel26@gmail.com>
2023-12-03 23:39:36 -0800

    Tested and fixed minor issue

--- reversed_words.js
+++ reversed_words.js
@@ -9,6 +9,8 @@ function reverseWords(str){
 
 console.log(reverseWords('I want to be great'));
 console.log(reverseWords('i love to eat bread and peanut butter'))
+console.log(reverseWords('I NEED TO solve more of these problems'))
+
 
 
 // IMPORTANT to note that using .reverse() permanently mutates the array we get from splitting the string. 
