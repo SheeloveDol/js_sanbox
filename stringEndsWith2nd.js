b8c42d3df4a97f208c16a65bbf871fa8ca0cc562
Sheelove Dol <siesel26@gmail.com>
2024-03-02 23:40:17 -0800

    Figured out solution along with simpler solution

--- stringEndsWith2nd.js
+++ stringEndsWith2nd.js
@@ -2,40 +2,29 @@ const stringEndsWith2nd = (str, ending) => {
   let sliceFromStr = [...str].slice(- ending.length);
   let arrayFromStr = [...sliceFromStr];
   let arrFromEnding = [...ending];
-  console.log(arrayFromStr);
+console.log(arrayFromStr);
 
-  if (arrayFromStr.length !==  arrFromEnding.length) return false;
+// if (arrayFromStr.length !==  arrFromEnding.length) return false;
+if (ending === '' && str !== '') {
+    return true;
+  } else if
 
-  for (let i = 0; i < arrayFromStr.length; i++) {
+for (let i = 0; i < arrayFromStr.length; i++) {
     if (arrayFromStr[i] !== arrFromEnding[i]) {
-        return false;
+        console.log(arrayFromStr[i], arrFromEnding[i]);
+            return false;
     };
     
-  }
+    return true;
+}
 
-  return true
-  ;
+  
 }
 
 
-console.log(stringEndsWith2nd('good', 's')) // abc
+console.log(stringEndsWith2nd('good', '')) // abc
 
 
 
-/* 
-function solution(str, ending){
-  let sliceFromStr = [...str].slice(-2);
-  let arrayFromStr = [...sliceFromStr]
-  let arrayFromEnding = [...ending];
-  
-  // Checks that both arrays are of equal length, if not then it's false.
-  if (arrayFromStr.length !== arrayFromEnding.length) return 'false';
-  
-  // Checks that each index from both arrays is equal
-  for (let i = 0; i < arrayFromStr.length; i++) {
-    if (arrayFromStr[i] !== arrayFromEnding[i]) return 'false'
-  }
-  
-  return 'true'
-}
-*/
+// the simplest solution is to use the built-in method endsWith() which returns a boolean value
+// return str.endsWith(ending); <---lol 
